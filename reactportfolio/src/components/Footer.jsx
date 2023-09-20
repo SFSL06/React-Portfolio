79c533ab5667a9d3fed02debf1ae689e54f7170f
Ammar Ozair <sabeenjamil100@yahoo.com>
2023-09-20 01:54:35 +0100

    Added contact details

---
+++ reactportfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+return (
+
+<footer id="footer">
+    <div class="container-fluid">
+       
+        <div class="contactDetails">
+        <p><a href="https://github.com/SFSL06">GitHub </a><b>Email: </b> xyz@yahoo.com   <b>Telephone Number:</b>123456789   <b>Address:</b>19 Monkspath Way Solihull United Kingdom</p>
+        </div>
+    </div>
+</footer>
+)}
+
+export default Footer;
